a00439c0dc0f86a956843dd238db75b88c3bea11
Camille CAMBIUM <ccambium@e1r5p7.clusters.42paris.fr>
2022-08-15 19:06:41 +0200

    creating copy env

--- minishell.c
+++ minishell.c
@@ -3,17 +3,42 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: gbertin <gbertin@student.42.fr>            +#+  +:+       +#+        */
+/*   By: ccambium <ccambium@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/15 12:31:18 by gbertin           #+#    #+#             */
-/*   Updated: 2022/08/15 12:32:24 by gbertin          ###   ########.fr       */
+/*   Updated: 2022/08/15 18:34:37 by ccambium         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "includes/minishell.h"
+#include <stdlib.h>
 
-int main(int argc, char **argv, char **envp)
+int	main(int argc, char **argv, char **envp)
 {
+	t_minishell	ms;
+	t_env		*e;
+	char		**s;
+	size_t		i;
 
-	return (0);
+	(void)argv;
+	(void)argc;
+	ms.o_head = NULL;
+	ms.e_head = NULL;
+	copy_env(&ms, envp);
+	s = env_to_tab(&ms);
+	i = -1;
+	while (s[++i])
+		printf("%s\n", s[i]);
+	e = ms.e_head;
+	while (e != NULL)
+	{
+		free(e->key);
+		free(e->value);
+		e = e->next;
+	}
+	i = -1;
+	while (s[++i])
+		free(s[i]);
+	free_all(&ms);
+	return (EXIT_SUCCESS);
 }
---
+++ sources/copy_env.c
@@ -0,0 +1,66 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   copy_env.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ccambium <ccambium@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/15 16:31:02 by ccambium          #+#    #+#             */
+/*   Updated: 2022/08/15 17:54:14 by ccambium         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/minishell.h"
+
+static char	*ft_superjoin(char **s)
+{
+	char	*ret_v;
+	char	*tmp;
+	size_t	i;
+
+	i = 0;
+	ret_v = NULL;
+	while (s[++i] != NULL)
+	{
+		tmp = ft_strjoin(ret_v, s[i]);
+		free(s[i]);
+		free(ret_v);
+		ret_v = tmp;
+		if (s[i + 1])
+		{
+			tmp = ft_strjoin(ret_v, "=");
+			free(ret_v);
+			ret_v = tmp;
+		}
+	}
+	return (ret_v);
+}
+
+void	copy_env(t_minishell *ms, char **env)
+{
+	size_t	i;
+	char	**s;
+	t_env	*e_tmp;
+	t_env	*last;
+
+	i = -1;
+	last = NULL;
+	if (env == NULL)
+		return ;
+	while (env[++i])
+	{
+		e_tmp = ft_malloc(sizeof(t_env), ms);
+		if (!e_tmp)
+			return ;
+		if (ms->e_head == NULL)
+			ms->e_head = e_tmp;
+		s = ft_split(env[i], '=');
+		e_tmp->key = s[0];
+		e_tmp->value = ft_superjoin(s);
+		free(s);
+		if (last != NULL)
+			last->next = e_tmp;
+		e_tmp->next = NULL;
+		last = e_tmp;
+	}
+}
