d902790524dd756bf9690b4ff6dc9cd80aace1c2
Camille CAMBIUM <ccambium@e1r7p12.clusters.42paris.fr>
2022-08-16 14:08:28 +0200

    renaming function

--- sources/utils/get_env_value.c
+++ sources/utils/get_env_value.c
@@ -1,18 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   env_var_to_value.c                                 :+:      :+:    :+:   */
+/*   get_env_value.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ccambium <ccambium@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/16 13:36:27 by ccambium          #+#    #+#             */
-/*   Updated: 2022/08/16 13:53:44 by ccambium         ###   ########.fr       */
+/*   Updated: 2022/08/16 14:07:49 by ccambium         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
 
-char	*env_var_to_value(char *key, t_minishell *ms)
+char	*get_env_value(char *key, t_minishell *ms)
 {
 	t_env	*env;
 
