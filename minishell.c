e6d0ea5c48c66fc3e8bc9932413824996eccf9a5
RoyaLProg <camille.cambium@gmail.com>
2022-08-17 10:13:42 +0200

    removing useless lines

--- minishell.c
+++ minishell.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: gbertin <gbertin@student.42.fr>            +#+  +:+       +#+        */
+/*   By: ccambium <ccambium@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/15 12:31:18 by gbertin           #+#    #+#             */
-/*   Updated: 2022/08/17 08:19:37 by gbertin          ###   ########.fr       */
+/*   Updated: 2022/08/17 10:10:02 by ccambium         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,6 @@
 int	main(int argc, char **argv, char **envp)
 {
 	t_minishell	ms;
-	t_env		*e;
-
-	ms.o_head = NULL;
-	ms.e_head = NULL;
 	char		*s;
 
 	(void)argv;
@@ -35,13 +31,6 @@ int	main(int argc, char **argv, char **envp)
 		free(s);
 	}
 	free(s);
-	e = ms.e_head;
-	while (e != NULL)
-	{
-		free(e->key);
-		free(e->value);
-		e = e->next;
-	}
 	free_all(&ms);
 	return (EXIT_SUCCESS);
 }
