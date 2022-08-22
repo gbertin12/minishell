854b6727617fc43d83579f0f906b74cffdc05ed4
Camille CAMBIUM <ccambium@e1r6p7.clusters.42paris.fr>
2022-08-22 11:11:09 +0200

    removing useless file + modifing  header

--- includes/built_in.h
+++ includes/built_in.h
@@ -6,7 +6,7 @@
 /*   By: ccambium <ccambium@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/16 09:58:16 by ccambium          #+#    #+#             */
-/*   Updated: 2022/08/19 19:52:43 by ccambium         ###   ########.fr       */
+/*   Updated: 2022/08/22 11:08:33 by ccambium         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@
 char	_cd(t_token *token, t_minishell *ms);
 int		_env(t_minishell *ms);
 int		_export(t_token *token, t_minishell *ms);
-int		_pwd(int fd);
+int		_pwd(t_token *token);
 int		_unset(char *key, t_minishell *ms);
 int		_echo(t_token *token);
 
