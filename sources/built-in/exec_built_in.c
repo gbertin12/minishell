7609a43dd42b59e7925f043ba4d569dc0d3b3f4c
gbertin12 <gaelbertin12@gmail.com>
2022-08-25 15:03:57 +0200

    add execute pwd

--- sources/built-in/exec_built_in.c
+++ sources/built-in/exec_built_in.c
@@ -6,7 +6,7 @@
 /*   By: gbertin <gbertin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/25 11:39:59 by gbertin           #+#    #+#             */
-/*   Updated: 2022/08/25 14:37:14 by gbertin          ###   ########.fr       */
+/*   Updated: 2022/08/25 15:02:54 by gbertin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,9 +33,31 @@ int	exec_env(t_token *token, t_minishell *ms)
 	return (1);
 }
 
+int	exec_pwd(t_token *token, t_minishell *ms)
+{
+	if (token->arg_head)
+		return (0);
+	if (token->next)
+	{
+		if (pipe(token->pipefd))
+			return (0);
+	}
+	token->pid = fork();
+	if (token->pid == 0)
+	{
+		init_execute(token, ms);
+		redir_out(token);
+		_pwd();
+		exit(0);
+	}
+	return (1);
+}
+
 int check_is_built_in(t_token *token, t_minishell *ms)
 {
 	if (ft_strcmp(token->cmd, "env"))
 		return (exec_env(token, ms));
+	if (ft_strcmp(token->cmd, "pwd"))
+		return (exec_pwd(token, ms));
 	return (0);
 }
