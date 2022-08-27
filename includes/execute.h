6615b7d4b2ca020b09ab4693b12994f18788cb79
gbertin12 <gaelbertin12@gmail.com>
2022-08-27 17:01:29 +0200

    fix merge

--- includes/execute.h
+++ includes/execute.h
@@ -6,7 +6,7 @@
 /*   By: gbertin <gbertin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/22 18:40:09 by gbertin           #+#    #+#             */
-/*   Updated: 2022/08/25 13:46:53 by gbertin          ###   ########.fr       */
+/*   Updated: 2022/08/27 16:30:56 by gbertin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,6 +26,13 @@ int		open_input(t_token *token, t_minishell *ms);
 int		open_output(t_token *token);
 int		have_infile(t_token *token);
 int		have_outfile(t_token *token);
+
+int		exec_echo(t_token *token, t_minishell *ms);
+int		exec_cd(t_token *token, t_minishell *ms);
+int		exec_pwd(t_token *token, t_minishell *ms);
+int		exec_env(t_token *token, t_minishell *ms);
+int		exec_export(t_token *token, t_minishell *ms);
+int		exec_unset(t_token *token, t_minishell *ms);
 
 int		redir_out(t_token *token);
 int		redir_in(t_token *token, t_token *last);
