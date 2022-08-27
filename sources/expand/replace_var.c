22c6461eaa89d49c66f5ebe93e900a5c8a26f23b
Camille CAMBIUM <ccambium@student.42.fr>
2022-08-27 14:41:25 +0200

    adding $? feature

--- sources/expand/replace_var.c
+++ sources/expand/replace_var.c
@@ -6,7 +6,7 @@
 /*   By: ccambium <ccambium@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/19 13:41:27 by ccambium          #+#    #+#             */
-/*   Updated: 2022/08/22 12:05:54 by ccambium         ###   ########.fr       */
+/*   Updated: 2022/08/27 14:39:28 by ccambium         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,10 @@ static char	*read_var(char *s, t_minishell *ms)
 	char	*ret_v;
 
 	i = 0;
+	if (s[i] == '?')
+		return (ft_strdup("?", ms));
+	if (!ft_isalpha(s[i]) && s[i] != '_')
+		return (ft_strdup("", ms));
 	while (s[i] && !is_space(s[i]) && s[i] != '$')
 		i++;
 	ret_v = ft_substr(s, 0, i, ms);
@@ -34,6 +38,8 @@ char	*replace_var(char *s, size_t x, t_minishell *ms)
 	if (!s || s[x] == 0)
 		return (s);
 	tmp2 = read_var(&s[x + 1], ms);
+	if (!tmp2 | !tmp2[0])
+		printf("$%c : not a valid identifier\n", s[x + 1]);
 	tmp = get_env_value(tmp2, ms);
 	ft_free(tmp2, ms);
 	if (!tmp)
@@ -45,7 +51,9 @@ char	*replace_var(char *s, size_t x, t_minishell *ms)
 	i = x + 1;
 	while (s[i] && ft_isalnum(s[i]))
 		i++;
-	printf("s[i] = %s |\n", &s[i]);
+	if (!ft_isalpha(s[x + 1]) && s[x + 1] != '_')
+		i = x + 2;
+	// printf("s[i] = %s |\n", &s[i]);
 	tmp = ft_substr(s, i, ft_strlen(&s[i]), ms);
 	ret_v = ft_strjoin(ret_v, tmp, ms);
 	ft_free(tmp, ms);
