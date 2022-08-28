09a3288716260abcc7220e50393f320ba1c2be0f
RoyaLProg <camille.cambium@gmail.com>
2022-08-28 12:17:51 +0200

    fixing parsing removing $

--- sources/parsing/search_next_sep.c
+++ sources/parsing/search_next_sep.c
@@ -6,7 +6,7 @@
 /*   By: ccambium <ccambium@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/17 17:15:55 by gbertin           #+#    #+#             */
-/*   Updated: 2022/08/27 16:41:22 by ccambium         ###   ########.fr       */
+/*   Updated: 2022/08/28 12:15:20 by ccambium         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ size_t	search_next_sep(char *s)
 	if (!s)
 		return (0);
 	while (s[i] != '|' && s[i] != '>' && s[i] != '<'
-		&& !ft_isalnum(s[i]) && s[i] != '\0')
+		&& !ft_isalnum(s[i]) && s[i] != '\0' && !is_space(s[i]))
 		i++;
 	return (i);
 }
