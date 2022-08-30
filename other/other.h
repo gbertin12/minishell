b79db024088762b5d9fedaa1cfb8c934832acd1a
RoyaLProg <royal@RoyalPC.localdomain>
2022-08-30 10:48:53 +0200

    Revert "m"
    This reverts commit 5848cd9712e7f3ae655ddfbdffbd5738e6fcf29f.

---
+++ other/other.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   other.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ccambium <ccambium@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/22 15:17:12 by ccambium          #+#    #+#             */
+/*   Updated: 2022/08/22 18:00:57 by ccambium         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef OTHER_H
+# define OTHER_H
+# include "../includes/minishell.h"
+# include <stdio.h>
+# include <stdlib.h>
+# include <stddef.h>
+
+
+#endif
