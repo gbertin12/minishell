eaaa52f935f94e62386a92c2926ac71291ee90f5
RoyaLProg <royal@RoyalPC.localdomain>
2022-09-12 15:51:18 +0200

    fixing expanding heredoc

--- sources/expand/expand.c
+++ sources/expand/expand.c
@@ -6,7 +6,7 @@
 /*   By: ccambium <ccambium@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/18 10:17:53 by ccambium          #+#    #+#             */
-/*   Updated: 2022/09/09 10:09:11 by ccambium         ###   ########.fr       */
+/*   Updated: 2022/09/12 15:24:56 by ccambium         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -55,6 +55,11 @@ static void	expand_file(t_token *token, t_minishell *ms)
 	file = token->file_head;
 	while (file)
 	{
+		if (file->type && file->append)
+		{
+			file = file->next;
+			continue ;
+		}
 		i = next_var(file->path);
 		while (file->path[i])
 		{
@@ -101,7 +106,7 @@ void	expand(t_minishell *ms)
 	{
 		expand_cmd(token, ms);
 		expand_arg(token, ms);
-		expand_file(token, ms);
+		// expand_file(token, ms); -> will be moce somewhere else
 		delete_quotes(token, ms);
 		token = token->next;
 	}
