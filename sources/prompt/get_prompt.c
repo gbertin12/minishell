39c53367e48d6a2eba9260844f76cf1a3c11556b
RoyaLProg <royal@RoyalPC.localdomain>
2022-09-23 09:35:26 +0200

    new prompt

--- sources/prompt/get_prompt.c
+++ sources/prompt/get_prompt.c
@@ -6,23 +6,18 @@
 /*   By: ccambium <ccambium@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/26 13:44:17 by ccambium          #+#    #+#             */
-/*   Updated: 2022/08/29 12:48:22 by ccambium         ###   ########.fr       */
+/*   Updated: 2022/09/23 09:34:11 by ccambium         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/prompt.h"
 
-static char	*get_prompt3(t_minishell *ms, char *ret_v, char *tmp)
+static char	*get_prompt2(t_minishell *ms, char *pwd, char *ret_v, char *tmp)
 {
-	tmp = ft_strjoin(ret_v, YELLOW, ms);
-	ft_free(ret_v, ms);
-	ret_v = ft_strjoin(tmp, "]", ms);
-	ft_free(tmp, ms);
-	tmp = ft_strjoin(ret_v, "\n", ms);
-	ft_free(ret_v, ms);
 	ret_v = ft_strjoin(tmp, BLUE, ms);
 	ft_free(tmp, ms);
-	tmp = ft_strjoin(ret_v, "└─", ms);
+	tmp = ft_strjoin(ret_v, pwd, ms);
+	ft_free(pwd, ms);
 	ft_free(ret_v, ms);
 	ret_v = ft_strjoin(tmp, string_ternary(ms->l_retv != 0, RED, GREEN), ms);
 	ft_free(tmp, ms);
@@ -33,26 +28,6 @@ static char	*get_prompt3(t_minishell *ms, char *ret_v, char *tmp)
 	return (ret_v);
 }
 
-static char	*get_prompt2(t_minishell *ms, char *pwd, char *ret_v, char *tmp)
-{
-	ret_v = ft_strjoin(tmp, ")", ms);
-	ft_free(tmp, ms);
-	tmp = ft_strjoin(ret_v, BLUE, ms);
-	ft_free(ret_v, ms);
-	ret_v = ft_strjoin(tmp, "──", ms);
-	ft_free(tmp, ms);
-	tmp = ft_strjoin(ret_v, YELLOW, ms);
-	ft_free(ret_v, ms);
-	ret_v = ft_strjoin(tmp, "[", ms);
-	ft_free(tmp, ms);
-	tmp = ft_strjoin(ret_v, GREEN, ms);
-	ft_free(ret_v, ms);
-	ret_v = ft_strjoin(tmp, pwd, ms);
-	free(pwd);
-	ft_free(tmp, ms);
-	return (get_prompt3(ms, ret_v, tmp));
-}
-
 char	*get_prompt(t_minishell *ms)
 {
 	char	*ret_v;
@@ -60,20 +35,12 @@ char	*get_prompt(t_minishell *ms)
 	char	*pwd;
 	char	*user;
 
-	pwd = NULL;
 	user = ft_strdup("minishell", ms);
 	pwd = getcwd(NULL, 0);
-	ret_v = ft_strjoin(BLUE, "┌──", ms);
-	tmp = ft_strjoin(ret_v, YELLOW, ms);
-	ft_free(ret_v, ms);
-	ret_v = ft_strjoin(tmp, "(", ms);
-	ft_free(tmp, ms);
-	tmp = ft_strjoin(ret_v, GREEN, ms);
-	ft_free(ret_v, ms);
-	ret_v = ft_strjoin(tmp, user, ms);
+	tmp = ft_strjoin(GREEN, user, ms);
 	ft_free(user, ms);
+	ret_v = ft_strjoin(tmp, WHITE, ms);
 	ft_free(tmp, ms);
-	tmp = ft_strjoin(ret_v, YELLOW, ms);
-	ft_free(ret_v, ms);
+	tmp = ft_strjoin(ret_v, ":", ms);
 	return (get_prompt2(ms, pwd, ret_v, tmp));
 }
