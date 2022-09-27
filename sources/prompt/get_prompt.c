b2beee3090debdce9b57ae169abbe4c3cdfd5521
Camille CAMBIUM <ccambium@e2r12p7.clusters.42paris.fr>
2022-09-27 13:05:00 +0200

    prompt

--- sources/prompt/get_prompt.c
+++ sources/prompt/get_prompt.c
@@ -6,7 +6,7 @@
 /*   By: ccambium <ccambium@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/26 13:44:17 by ccambium          #+#    #+#             */
-/*   Updated: 2022/09/23 09:34:11 by ccambium         ###   ########.fr       */
+/*   Updated: 2022/09/27 11:16:41 by ccambium         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,8 @@
 
 static char	*get_prompt2(t_minishell *ms, char *pwd, char *ret_v, char *tmp)
 {
+	if (do_env_key_exist("PS1", ms))
+		return (get_env_value("PS1", ms));
 	ret_v = ft_strjoin(tmp, BLUE, ms);
 	ft_free(tmp, ms);
 	tmp = ft_strjoin(ret_v, pwd, ms);
