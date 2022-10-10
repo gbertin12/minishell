4f028de4ae429a00e3546cef6bf1be9f9643244b
Camille CAMBIUM <ccambium@e1r2p19.clusters.42paris.fr>
2022-10-10 16:51:03 +0200

    cleaning

--- sources/execute/exit_child.c
+++ sources/execute/exit_child.c
@@ -6,11 +6,7 @@
 /*   By: ccambium <ccambium@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/05 11:45:11 by gbertin           #+#    #+#             */
-<<<<<<< HEAD
-/*   Updated: 2022/10/10 16:23:49 by gbertin          ###   ########.fr       */
-=======
-/*   Updated: 2022/10/10 16:02:36 by ccambium         ###   ########.fr       */
->>>>>>> f775a1edb2b7ae412141bc597c085993b9f4f381
+/*   Updated: 2022/10/10 16:50:37 by ccambium         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +16,7 @@ void	exit_child(int num_err, t_minishell *ms)
 {
 	if (ms->exec->tmpin)
 		close(ms->exec->tmpin);
-	if (ms->exec->tmpout)	
+	if (ms->exec->tmpout)
 		close(ms->exec->tmpout);
 	free(ms->s);
 	free_all(ms);
