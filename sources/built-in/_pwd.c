e8a567b6ff96b09e6188c59ce859e3bf9483a831
RoyaLProg <royal@RoyalPC.localdomain>
2022-10-12 17:19:43 +0200

    fixing pwd too many argument

--- sources/built-in/_pwd.c
+++ sources/built-in/_pwd.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   _pwd.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: gbertin <gbertin@student.42.fr>            +#+  +:+       +#+        */
+/*   By: ccambium <ccambium@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/16 21:00:27 by gbertin           #+#    #+#             */
-/*   Updated: 2022/10/11 14:50:30 by gbertin          ###   ########.fr       */
+/*   Updated: 2022/10/12 17:19:29 by ccambium         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,6 @@
 
 int	_pwd(t_token *token, t_minishell *ms)
 {
-	if (count_arg(token->arg_head))
-	{
-		ft_putstr_fd("minishell: pwd: too many arguments\n", 2);
-		return (1);
-	}
 	ft_putstr_fd(ms->pwd, 1);
 	ft_putchar_fd('\n', 1);
 	return (EXIT_SUCCESS);
