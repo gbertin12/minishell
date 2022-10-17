bae6cf784d4d5991d332aa1ea7060183e08e715f
Gael BERTIN <gbertin@e1r1p1.clusters.42paris.fr>
2022-10-17 12:42:00 +0200

    fix ret err sig

--- sources/execute/browse_cmd.c
+++ sources/execute/browse_cmd.c
@@ -6,7 +6,7 @@
 /*   By: gbertin <gbertin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/23 16:20:32 by gbertin           #+#    #+#             */
-/*   Updated: 2022/10/11 10:18:14 by gbertin          ###   ########.fr       */
+/*   Updated: 2022/10/17 12:41:30 by gbertin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -72,7 +72,12 @@ int	end_browse_cmd(t_exec *exec, t_minishell *ms)
 		exec->token = exec->token->next;
 	}
 	if (have_child(ms->t_head))
-		exec->l_retv = WEXITSTATUS(status);
+	{
+		if (WIFSIGNALED(status))
+			exec->l_retv = status;
+		else
+			exec->l_retv = WEXITSTATUS(status);
+	}
 	if (dup2(exec->tmpin, 0) == -1)
 		return (1);
 	if (dup2(exec->tmpout, 1) == -1)
