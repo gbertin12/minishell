a9b8bd0abfe29c905f1d3edb279e742c21e48b52
Camille CAMBIUM <ccambium@e2r2p15.clusters.42paris.fr>
2022-11-03 16:06:47 +0100

    new expand_arg.c

--- sources/expand/expand_arg.c
+++ sources/expand/expand_arg.c
@@ -6,110 +6,93 @@
 /*   By: ccambium <ccambium@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/09 10:09:49 by ccambium          #+#    #+#             */
-/*   Updated: 2022/10/31 13:03:24 by ccambium         ###   ########.fr       */
+/*   Updated: 2022/11/03 16:04:35 by ccambium         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
 
-static char	split_arg2(char **argv, t_minishell *ms,
-			t_arg **first, t_arg **last)
+static void	split_arg(t_arg *arg, t_minishell *ms)
 {
-	size_t	i;
-	t_arg	*a;
+	int		i;
+	char	**split;
 
-	i = 0;
-	a = ft_malloc(sizeof(t_arg), ms);
-	if (!a)
-		return (1);
-	*first = a;
-	while (argv[++i])
+	split = ft_split_set(arg->value, " \n\r\v\t\f", ms);
+	if (!split || !split[0])
+		return ;
+	ft_free(arg->value, ms);
+	arg->value = split[0];
+	if (!split[1])
+		return ;
+	i = count_tab(split) - 1;
+	while (i > 0)
 	{
-		a->value = ft_strdup(argv[i], ms);
-		if (argv[i + 1])
+		arg->next = add_front_arg(arg->next, split[i], ms);
+		if (arg->next == NULL)
 		{
-			a->next = ft_malloc(sizeof(t_arg), ms);
-			if (!a->next)
-				return (1);
-			a = a->next;
-			continue ;
+			free_split(split, ms);
+			return ;
 		}
-		else
-			break ;
-	}
-	*last = a;
-	return (0);
-}
-
-char	split_arg(t_arg *arg, t_minishell *ms)
-{
-	char	**arg_value;
-	t_arg	*last;
-	t_arg	*first;
-
-	first = NULL;
-	last = NULL;
-	if (!arg->value)
-		return (0);
-	arg_value = ft_split_set(arg->value, " \n\r\v\t\f", ms);
-	if (!arg_value)
-		return (1);
-	if (split_arg2(arg_value, ms, &first, &last))
-		return (free_split(arg_value, ms), 1);
-	arg->value = arg_value[0];
-	last->next = arg->next;
-	arg->next = first;
-	return (free_split(arg_value, ms), 0);
-}
-
-static void	expand_arg2(t_token *token, t_minishell *minishell)
-{
-	t_arg	*arg;
-
-	arg = token->arg_head;
-	while (arg)
-	{
-		if (!arg->value)
-		{
-			arg = arg->next;
-			continue ;
-		}
-		arg->value = remove_quotes(arg->value, minishell, 0);
-		split_arg(arg, minishell);
-		arg = arg->next;
+		i--;
 	}
 }
 
-static char	*next_var(char *s)
+static size_t	next_var(char *s)
 {
 	size_t	i;
 
 	i = 0;
-	while (s[i] && s[i] != '$' && !between_quote(s, i))
+	while ((s && s[i]) || between_quote(s, i))
+	{
+		if (s[i] == '$' && !between_quote(s, i) && s[i + 1] != '\0'
+			&& !is_space(s[i + 1]) && s[i + 1] != '$')
+			return (i);
 		i++;
-	if (!s[i])
-		return (NULL);
-	return (s + i);
+	}
+	return (i);
+}
+
+static char	in_quote(char *s, size_t x)
+{
+	char	d_quote;
+	char	quote;
+	size_t	i;
+
+	quote = 0;
+	d_quote = 0;
+	i = 0;
+	while (s && s[i] && i <= x)
+	{
+		if (s[i] == '"' && !quote)
+			d_quote = char_ternary(d_quote, 0, 1);
+		if (s[i] == '\'' && !d_quote)
+			quote = char_ternary(quote, 0, 1);
+		i++;
+	}
+	return (quote + d_quote);
 }
 
 char	expand_arg(t_token *token, t_minishell *ms)
 {
+	size_t	i;
 	t_arg	*arg;
-	char	*i;
 
 	arg = token->arg_head;
+	if (arg == NULL)
+		return (0);
 	while (arg)
 	{
 		i = next_var(arg->value);
-		if (!i || !*i || i[1] == '\0' || is_space(i[1]) || i[1] == '$')
+		if (!arg->value || !arg->value[i])
 		{
 			arg = arg->next;
 			continue ;
 		}
-		arg->value = replace_var(arg->value, i - arg->value, ms);
+		arg->value = replace_var(arg->value, i, ms);
 		if (!arg->value)
 			return (1);
+		if (!in_quote(arg->value, i))
+			split_arg(arg, ms);
 	}
-	expand_arg2(token, ms);
-	return (0);
+	return (EXIT_SUCCESS);
 }
